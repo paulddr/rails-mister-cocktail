b9c422d6fe51be7ddb337c19f35a22d54aa49721
paul didier <paul--didier@live.fr>
2020-05-29 12:10:11 +0200

    Add dotenv - Protect my secret data in .env file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_101738) do
+ActiveRecord::Schema.define(version: 2020_05_28_143027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_101738) do
   end
 
   create_table "doses", force: :cascade do |t|
-    t.text "description"
+    t.string "description"
     t.bigint "cocktail_id", null: false
     t.bigint "ingredient_id", null: false
     t.datetime "created_at", precision: 6, null: false
